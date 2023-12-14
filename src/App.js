3ad7781c2427b9da9804cc2a9d3fe2909e191f7f
Mazalee <108081347+Mazalee@users.noreply.github.com>
2023-12-14 22:14:15 +0100

    faq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import pic from './images/pic.jpg'
+import { questions }  from './Questions';
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowUp } from "react-icons/md";
 
 function App() {
+
+  const [faqs, setFaqs] = useState(questions);
+
+  const [icon, setIcon] = useState(true);
+
+  const handleClick = () => {
+    setIcon(!icon);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className="center">
+        <div className="left">
+          <figure>
+            <img src={pic} alt='pic' />
+          </figure>
+        </div>
+        <div className="right">
+          <h1>FAQ</h1>
+          {
+            faqs.map((faq) => {
+              const { question, answer, id } = faq;
+              return (
+                <div className='inner_right' key={id}>
+                  <div className='questions' onClick={handleClick}>
+                    <h3>{question}</h3>
+                    <span>{icon ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />}</span>
+                  </div>
+                  {icon && <div className='answers'><p>{answer}</p></div>}
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Questions.js
@@ -0,0 +1,28 @@
+export const questions = [
+    {
+        id: 1,
+        question: 'How many team members can I invite?',
+        answer: 'No more than 2GB. All files in your account must fit your alloted storage space.',
+        
+    },
+    {
+        id: 2,
+        question: 'What is the maximum file upload size?',
+        answer: '',
+    },
+    {
+        id: 3,
+        question: 'How do I reset my password?',
+        answer: '',
+    },
+    {
+        id: 4,
+        question: 'Can I cancel my subscription?',
+        answer: '',
+    },
+    {
+        id: 5,
+        question: 'Do you provide additional support?',
+        answer: ''
+    },
+]
